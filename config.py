1738cc110bdfb88ff5422737628d48b2a5abe585
Sudeep Hiremath <73274558+subeep@users.noreply.github.com>
2025-03-11 05:08:55 +0530

    setup

---
+++ config.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+def get_config():
+    return {
+        'batch_size': 16,
+        'seq_len': 350,
+        'd_model': 512,
+        'learning_rate': 0.0001,
+        'num_epochs': 20,
+        'lang_src': 'en',
+        'lang_tgt': 'it',
+        'model_folder': 'weights',
+        'model_filename': 'tmodel_',
+        'preload': None,
+        'tokenizer_file':'tokenizer_{0}.json',
+        'experiment_name': 'runs/tmodel'
+    }
+
+
+def get_weights_file_path(config, epoch:str):
+    model_folder = config['model_folder']
+    model_basename = config['model_basename']
+    model_filename = f"{model_basename}{epoch}.pt"
+    return str(path(' .') / model_folder / model_filename)
