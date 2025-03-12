5368212f06ed246d0988c2e4974ee3f38db181f5
Sudeep Hiremath <73274558+subeep@users.noreply.github.com>
2025-03-12 23:08:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,23 +2,31 @@ from pathlib import Path
 
 def get_config():
     return {
-        'batch_size': 16,
-        'seq_len': 350,
-        'd_model': 512,
-        'learning_rate': 0.0001,
-        'num_epochs': 20,
-        'lang_src': 'en',
-        'lang_tgt': 'it',
-        'model_folder': 'weights',
-        'model_filename': 'tmodel_',
-        'preload': None,
-        'tokenizer_file':'tokenizer_{0}.json',
-        'experiment_name': 'runs/tmodel'
+        "batch_size": 8,
+        "num_epochs": 20,
+        "lr": 0.0001,
+        "seq_len": 200,
+        "d_model": 512,
+        "datasource": 'opus_books',
+        "lang_src": "en",
+        "lang_tgt": "it",
+        "model_folder": "weights",
+        "model_basename": "tmodel_",
+        "preload": "latest",
+        "tokenizer_file": "tokenizer_{0}.json",
+        "experiment_name": "runs/models"
     }
 
+def get_weights_file_path(config, epoch: str):
+    model_folder = f"{config['datasource']}_{config['model_folder']}"
+    model_filename = f"{config['model_basename']}{epoch}.pt"
+    return str(Path('.') / model_folder / model_filename)
 
-def get_weights_file_path(config, epoch:str):
-    model_folder = config['model_folder']
-    model_basename = config['model_basename']
-    model_filename = f"{model_basename}{epoch}.pt"
-    return str(path(' .') / model_folder / model_filename)
+def latest_weights_file_path(config):
+    model_folder = f"{config['datasource']}_{config['model_folder']}"
+    model_filename = f"{config['model_basename']}*"
+    weights_files = list(Path(model_folder).glob(model_filename))
+    if len(weights_files) == 0:
+        return None
+    weights_files.sort()
+    return str(weights_files[-1])
